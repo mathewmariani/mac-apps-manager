e38a204a6bb6ecc4f1f15a3554652557130c55cf
matty <someone.mariani@gmail.com>
2015-07-24 17:56:06 -0400

    initial commit

---
+++ assets/js/blog.js
@@ -0,0 +1,20 @@
+var blog = angular.module('blogApp', []);
+
+blog.controller('FormController', function ($scope, $http, $location) {
+    console.log('Form Controller Initialized');
+
+    // @TODO Toggle All needs to be implemented
+    $scope.toggle = function () {
+        console.log('toggle');
+    };
+
+    $scope.submit = function () {
+        console.log($scope.apps);
+
+        for (app in $scope.apps) {
+            if ($scope.apps[app]) {
+                console.log('curl -s ' + $location.host() + '/_includes/' + app + ' | sh');
+            }
+        }
+    };
+});
